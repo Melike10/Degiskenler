1d2f7acad8efae305a03084141a19b7dae085974
Melike Demir Göz <demirmelike187@gmail.com>
2024-07-16 23:55:53 +0300

    Update Program.cs

--- Degiskenler/Program.cs
+++ Degiskenler/Program.cs
@@ -1,19 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Lütfen aşağıdaki bilgileri giriniz!");
 Console.Write("T.C. Kimlik Numarası:"  );
-// int değerinin değer aralığını geçtiği için string kullandım
-string TcNo = Console.ReadLine();
+// int değerinin değer aralığını geçtiği için double kullandım
+double TcNo = Convert.ToDouble(Console.ReadLine());
 Console.Write("\n Adı :");
 string ad = Console.ReadLine();
 Console.Write("\n Soyadı :");
 string soyAd = Console.ReadLine();
-// baştakı sıfır girlmezse 10 haneli yani int değeri içinde oluyor o yüzden int kullandım ve böylece 10 hane ile sınırladım.
+// baştaki sıfır girilmese de 10 hane olduğu ve int sınırını geçtiği için double kullandım  .
 Console.Write("\n Telefon :");
-int phone = Convert.ToInt32(Console.ReadLine());
+double phone = Convert.ToDouble(Console.ReadLine());
 // byte kullandım yaş için çünkü bir insan ömrü  256'dan küçüktür.
 Console.Write("\n Yaş :");
 byte yas = Convert.ToByte(Console.ReadLine());
-// para da bir kısıt görmediğim için canlı derste öğrendiğim paaralar için en iyi tip olan decimali kullandım. 
+// para da bir kısıt görmediğim için (2 milyarı da geçebileceğini düşünerek) canlı derste öğrendiğim paralar için en iyi tip olan decimali kullandım. 
 Console.Write("\n İlk Aldığı Ürünün Fiyatı :");
 decimal f1 = Convert.ToDecimal(Console.ReadLine());
 Console.Write("\n İkinci Aldığı Ürünün Fiyatı :");
